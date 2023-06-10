adb188788207631031436bdcde1864a4a7d0a1dd
lpavan2002 <126876542+lpavan2002@users.noreply.github.com>
2023-06-10 10:11:31 +0530

    Update Task 1.py

--- Task 1.py
+++ Task 1.py
@@ -1,114 +1,55 @@
+TASK1:
+def print_board(board):
+    print(f" {board[0]} | {board[1]} | {board[2]} ")
+    print("-----------")
+    print(f" {board[3]} | {board[4]} | {board[5]} ")
+    print("-----------")
+    print(f" {board[6]} | {board[7]} | {board[8]} ")
 
-import random
-board = ["-", "-", "-",
-        "-", "-", "-",
-        "-", "-", "-"]
-currentPlayer = "X"
-winner = None
-gameRunning = True
-# game board
-def printBoard(board):
-    print(board[0] + " | " + board[1] + " | " + board[2])
-    print("---------")
-    print(board[3] + " | " + board[4] + " | " + board[5])
-    print("---------")
-    print(board[6] + " | " + board[7] + " | " + board[8])
-    
-# take player input
-def playerInput(board):
-    inp = int(input("Select a position 1-9: "))
-    if board[inp-1] == "-":
-        board[inp-1] = currentPlayer
-    else:
-        print("Sorry player is already at that position.")
-        
-# take player input
-def playerInput(board):
-    inp = int(input("Select a position 1-9: "))
-    if board[inp-1] == "-":
-        board[inp-1] = currentPlayer
-    else:
-        print("Sorry player is already at that position.")
-     
+def is_winner(board, player):
+    return ((board[0] == player and board[1] == player and board[2] == player) or
+            (board[3] == player and board[4] == player and board[5] == player) or
+            (board[6] == player and board[7] == player and board[8] == player) or
+            (board[0] == player and board[3] == player and board[6] == player) or
+            (board[1] == player and board[4] == player and board[7] == player) or
+            (board[2] == player and board[5] == player and board[8] == player) or
+            (board[0] == player and board[4] == player and board[8] == player) or
+            (board[2] == player and board[4] == player and board[6] == player))
 
-# check for win or tie
-def checkHorizontle(board):
-    global winner
-    if board[0] == board[1] == board[2] and board[0] != "-":
-        winner = board[0]
-        return True
-    elif board[3] == board[4] == board[5] and board[3] != "-":
-        winner = board[3]
-        return True
-    elif board[6] == board[7] == board[8] and board[6] != "-":
-        winner = board[6]
-        return True
-        
-def checkRow(board):
-    global winner
-    if board[0] == board[3] == board[6] and board[0] != "-":
-        winner = board[0]
-        return True
-    elif board[1] == board[4] == board[7] and board[1] != "-":
-        winner = board[1]
-        return True
-    elif board[2] == board[5] == board[8] and board[2] != "-":
-        winner = board[3]
-        return True
-        def checkDiag(board):
-    global winner
-    if board[0] == board[4] == board[8] and board[0] != "-":
-        winner = board[0]
-        return True
-    elif board[2] == board[4] == board[6] and board[4] != "-":
-        winner = board[2]
-        return True
-        def checkIfWin(board):
-    global gameRunning
-    if checkHorizontle(board):
-        printBoard(board)
-        print(f"The winner is {winner}!")
-        gameRunning = False
+def is_board_full(board):
+    return all(x != " " for x in board)
 
-    elif checkRow(board):
-        printBoard(board)
-        print(f"The winner is {winner}!")
-        gameRunning = False
+def get_valid_input(board):
+    while True:
+        user_input = input("Enter a position (1-9): ")
+        if not user_input.isdigit():
+            print("Please enter a valid number.")
+        elif int(user_input) < 1 or int(user_input) > 9:
+            print("Please enter a number between 1 and 9.")
+        elif board[int(user_input) - 1] != " ":
+            print("That position is already taken.")
+        else:
+            return int(user_input) - 1
 
-    elif checkDiag(board):
-        printBoard(board)
-        print(f"The winner is {winner}!")
-        gameRunning = False
-        
-def checkIfTie(board):
-    global gameRunning
-    if "-" not in board:
-        printBoard(board)
-        print("It is a tie!")
-        gameRunning = False
-        
-# switch the player
-def switchPlayer():
-    global currentPlayer
-    if currentPlayer == "X":
-        currentPlayer = "O"
-    else:
-        currentPlayer = "X"
-        
-def computer(board):
-    while currentPlayer == "O":
-        position = random.randint(0, 8)
-        if board[position] == "-":
-            board[position] = "O"
-            switchPlayer()
+def play_game():
+    board = [" "] * 9
+    players = ["X", "O"]
+    current_player = players[0]
 
-#check for win or tie again
-while gameRunning:
-    printBoard(board)
-    playerInput(board)
-    checkIfWin(board)
-    checkIfTie(board)
-    switchPlayer()
-    computer(board)
-    checkIfWin(board)
-    checkIfTie(board)
+    while True:
+        print_board(board)
+        print(f"Player {current_player}'s turn.")
+        position = get_valid_input(board)
+        board[position] = current_player
+        if is_winner(board, current_player):
+            print_board(board)
+            print(f"Player {current_player} wins!")
+            break
+        elif is_board_full(board):
+            print_board(board)
+            print("The game is a tie!")
+            break
+        else:
+            current_player = players[1] if current_player == players[0] else players[0]
+
+play_game()
